b5fa3d665b653b4da0aa0cc315fbc352ff1b0108
Pasindu Dilshan Dimbulana <pasindudi@mobitel.lk>
2024-12-03 15:48:15 +0530

    initial commit

---
+++ src/main/java/com/spring/security/springbootsecurityi/SpringBootSecurityIApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.springbootsecurityi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/security/springbootsecurityi/SpringBootSecurityIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.springbootsecurityi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
